76f5c5ee6e02ba97ab622084f3048924d359f26c
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 04:06:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,9 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  for for_iterator in 1..number_of_times 
+  for for_iterator in 1..number_of_times do
+    puts phrase
+  end
   
 end
 
